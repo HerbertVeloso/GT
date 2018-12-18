b3bb74e648c00a75dade1576c10e0d557a6a47ac
HerbertVeloso <herbert.oliveira.veloso@gmail.com>
2018-12-18 01:01:55 -0200

    CSS and JS basic

---
+++ js/scroll.js
@@ -0,0 +1,10 @@
+// Navegação da página
+$('.scroll').click(function(event){
+    event.preventDefault();
+    var id = $(this).attr('href'),
+        targetOffset = $(id).offset().top,
+        menuHeight = $('.menu').innerHeight();
+    $('html, body').animate({
+        scrollTop: targetOffset - menuHeight
+    }, 500);
+});
